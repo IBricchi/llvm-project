4aad17f59d5622a2893197d88a7277eeae4de0a8
Theodoros Theodoridis <theodort@inf.ethz.ch>
2022-12-19 16:23:04 +0100

    [InlineOrder] Plugin Inline Order
    Adds the ability to load a plugin to control the inline order.
    This allows developing and distributing inlining heuristics
    outside of tree. And together with the inline advisor plugins
    allows for fine grained control of the inliner.
    
    The PluginInlineOrderAnalysis class serves as the entry point
    for dynamic advisors. Plugins must register instances of this
    class to provide their own InlineOrder.
    
    Differential Revision: https://reviews.llvm.org/D140637

--- llvm/include/llvm/Analysis/InlineOrder.h
+++ llvm/include/llvm/Analysis/InlineOrder.h
@@ -32,7 +32,86 @@ public:
 };
 
 std::unique_ptr<InlineOrder<std::pair<CallBase *, int>>>
-getInlineOrder(FunctionAnalysisManager &FAM, const InlineParams &Params);
+getInlineOrder(FunctionAnalysisManager &FAM, const InlineParams &Params,
+               ModuleAnalysisManager &MAM, Module &M);
+
+/// Used for dynamically registering InlineOrder as plugins
+///
+/// An inline order plugin adds a new inline order at runtime by registering
+/// an instance of PluginInlineOrderAnalysis in the current
+/// ModuleAnalysisManager. For example, the following code dynamically registers
+/// the default inline order:
+///
+/// namespace {
+///
+/// std::unique_ptr<InlineOrder<std::pair<CallBase *, int>>>
+/// DefaultInlineOrderFactory(FunctionAnalysisManager &FAM,
+///                           const InlineParams &Params,
+///                           ModuleAnalysisManager &MAM, Module &M) {
+///   llvm::PluginInlineOrderAnalysis::HasBeenRegistered = false;
+///   auto DefaultInlineOrder =
+///       std::unique_ptr<InlineOrder<std::pair<CallBase *, int>>>(
+///           getInlineOrder(FAM, Params, MAM, M));
+///   llvm::PluginInlineOrderAnalysis::HasBeenRegistered = true;
+///   return DefaultInlineOrder;
+/// }
+///
+/// struct DefaultDynamicInlineOrder : PassInfoMixin<DefaultDynamicInlineOrder>
+/// {
+///   PreservedAnalyses run(Module &M, ModuleAnalysisManager &MAM) {
+///     PluginInlineOrderAnalysis DA(DefaultInlineOrderFactory);
+///     MAM.registerPass([&] { return DA; });
+///     return PreservedAnalyses::all();
+///   }
+/// };
+///
+/// } // namespace
+///
+/// extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+/// llvmGetPassPluginInfo() {
+///   return {LLVM_PLUGIN_API_VERSION, "DefaultDynamicInlineOrder",
+///   LLVM_VERSION_STRING,
+///           [](PassBuilder &PB) {
+///             PB.registerPipelineStartEPCallback(
+///                 [](ModulePassManager &MPM, OptimizationLevel Level) {
+///                   MPM.addPass(DefaultDynamicInlineOrder());
+///                 });
+///           }};
+/// }
+///
+/// A plugin must implement an InlineOrderFactory and register it with a
+/// PluginInlineOrderAnalysis to the provided ModuleanAlysisManager.
+///
+/// If such a plugin has been registered
+/// llvm::getInlineOrder will return the dynamically loaded inliner order.
+///
+class PluginInlineOrderAnalysis
+    : public AnalysisInfoMixin<PluginInlineOrderAnalysis> {
+public:
+  static AnalysisKey Key;
+  static bool HasBeenRegistered;
+
+  typedef std::unique_ptr<InlineOrder<std::pair<CallBase *, int>>> (
+      *InlineOrderFactory)(FunctionAnalysisManager &FAM,
+                           const InlineParams &Params,
+                           ModuleAnalysisManager &MAM, Module &M);
+
+  PluginInlineOrderAnalysis(InlineOrderFactory Factory) : Factory(Factory) {
+    HasBeenRegistered = true;
+    assert(Factory != nullptr &&
+           "The plugin inline order factory should not be a null pointer.");
+  }
+
+  struct Result {
+    InlineOrderFactory Factory;
+  };
+
+  Result run(Module &, ModuleAnalysisManager &) { return {Factory}; }
+  Result getResult() { return {Factory}; }
+
+private:
+  InlineOrderFactory Factory;
+};
 
 } // namespace llvm
 #endif // LLVM_ANALYSIS_INLINEORDER_H
